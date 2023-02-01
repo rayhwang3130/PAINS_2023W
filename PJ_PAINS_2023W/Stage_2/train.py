5320047616b6d2dc1127cb6fe6f18d536d7ffa9a
Seung Hun Han <andrewhan@korea.ac.kr>
2023-02-02 01:12:24 +0900

    Delete PJ_PAINS_2023W directory

--- PJ_PAINS_2023W/Stage_2/train.py
+++
@@ -1,9 +0,0 @@
-def train(model, train_data, hyperparameters, **kwargs):
-
-    model.fit(
-        train_data=train_data,
-        # hyperparameters = hyperparameters,
-        **kwargs
-    )
-
-    return model
--- PJ_PAINS_2023W/footballanalysis/Bird/run.sh
+++
@@ -1,9 +0,0 @@
-python new_main.py \
-    --df './Records.csv' \
-    --df_name 'Processed_records' \
-    --output_dir './inference/output/train/' \
-    --crop_dir './inference/cropped/train/' \
-    --black_dir './inference/black.jpg' \
-    --img_dir './image/train/' \
-    --weight './weights/yolov5s.pt' \
-    --near_param 50
